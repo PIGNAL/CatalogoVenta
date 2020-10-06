fd6675fb0e40146d22ca4ee32d16c558d3abe200
PIGNAL <joni_ballatore@hotmail.com>
2020-10-06 16:23:47 -0300

    Create Domain

--- Domain/Properties/AssemblyInfo.cs
+++ Domain/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Negocio")]
+[assembly: AssemblyTitle("Domain")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Negocio")]
+[assembly: AssemblyProduct("Domain")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e25dfb74-d7c3-45bb-916f-ee470f7ea1fb")]
+[assembly: Guid("b2d24863-3702-4248-b403-8ed7009e077c")]
 
 // Version information for an assembly consists of the following four values:
 //
