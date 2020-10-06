859d9433535c589d362c0b76d5d444b06d7d69d0
PIGNAL <joni_ballatore@hotmail.com>
2020-10-06 16:09:55 -0300

    Database design

--- CatalogoVenta/ClientApp/src/app/app.module.ts
+++ CatalogoVenta/ClientApp/src/app/app.module.ts
@@ -5,15 +5,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent
@@ -26,7 +25,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
